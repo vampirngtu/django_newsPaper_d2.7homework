65ecc5058795a31cef45b3ee2c5dd13ba3b3d84e
Andrei <121810915+vampirngtu@users.noreply.github.com>
2023-06-25 20:11:10 +0700

    доделывал по видео
    Миграция сработала, впринципе все должно работать...

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -7,53 +7,47 @@ from django.db.models import Sum
 PREVIEW_LEN = 124
 
 class Author(models.Model):
-    author_name = models.CharField(max_length=255)
-    reit = models.IntegerField(default=0)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
+    ratingAuthor = models.IntegerField(default=0)
 
-    def update_rating(self): #суммарный рейтинг каждой статьи автора умножается на 3;
-                            #суммарный рейтинг всех комментариев автора;
-                            #суммарный рейтинг всех комментариев к статьям автора.
-        n1 = Post.objects.filter(author=self.id).aggregate(rating=Sum('reit'))['reit'] * 3
+    def update_rating(self):
+        postRat = self.post_set.all().aggregate(postRating=Sum('rating'))
+        pRat = 0
+        pRat +=postRat.get('postRating')
 
-        cr = Comment.objects.filter(user=self.user).aggregate(comments_rating=Sum('reit_Commit'))
-        if not cr['reit_Commit'] is None:
-            n1 += cr['reit_Commit']
+        commentRat = self.authorUser.comment_set.all().aggregate(commentsRating=Sum('rating'))
+        cRat = 0
+        cRat +=commentRat.get('commentRating')
 
-        for i in Post.objects.filter(author=self.id, post_type='0'):
-            n1 += Comment.objects.filter(post=i).aggregate(post_rating=Sum('reit'))['reit_Commit']
-
-        self.reit = n1
+        self.ratingAuthor = pRat * 3 + cRat
         self.save()
-        return n1
 
 class Category(models.Model):
-    TEMAS = [
-        (sport, 'спорт'),
-        (politik, 'политика'),
-        (knowlength, 'образование'),
-        (biznes, 'бизнес'),
-        (other, 'разное'),
-    ]
-    TEMA = models.CharField(max_length=255, choices=TEMAS, default=other, unique=True)
-
-POST_TYPE = [('0', 'статья'), ('1', 'новость')]
+    name = models.CharField(max_length=64, unique=True)
 
 class Post(models.Model):
-    post_type = models.CharField(max_length=1, choices=POST_TYPE, default='0')
-    time_in = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    POST_TYPE = (
+        (ARTICLE, 'статья'),
+        (NEWS, 'новость'),
+    )
+    post_type = models.CharField(max_length=2, choices=POST_TYPE, default=ARTICLE)
+    time_in = models.DateTimeField(auto_now_add=True)
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
 
     zagolovok = models.CharField(max_length=255)
     novosti = models.TextField(default="Новость в разработке")
-    reit_Novosti = model.IntegerField(default=0)
-    #Post = models.ManyToManyField(Author, through='ProductOrder')
+    rating = models.IntegerField(default=0)
+
 
     def like(self):
-        self.reit_Novosti += 1
+        self.rating += 1
 
     def dislike(self):
-        self.reit_Novosti -= 1
+        self.rating -= 1
      #возвращает начало статьи (предварительный
                         # просмотр) длиной 124 символа и
                          # добавляет многоточие в конце.
@@ -64,20 +58,23 @@ def preview(self):
         return self.novosti
 
 class PostCategory(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.DO_NOTHING)
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
+    categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
     komment = models.TextField(default="без комментариев")
     time_Commit = models.DateTimeField(auto_now_add=True)
-    reit_Commit = model.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+       return self.commentUser.username
 
     def like(self):
-        self.reit_Commit += 1
+        self.rating += 1
 
     def dislike(self):
-        self.reit_Commit -= 1
+        self.rating -= 1
 
 
