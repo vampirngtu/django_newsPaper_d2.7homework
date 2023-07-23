dcd7167caf7478676ede6d0f4a800c3dd5cd8288
Andrei <121810915+vampirngtu@users.noreply.github.com>
2023-07-23 15:01:57 +0700

    с запуском News помог ментор
    Полность перепроходил уроки D1-3, News новый, но когда заливаеш как новый комент, какие то старые файлы в репозитории сохраняются, те которые не перезаписывались, что может повлиять на работоспособность.
    Значения матов заменяются на звездочки. отпустите меня уже с фильтров, не могу оставить не доделанным и уйти дальше...

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -1,18 +1,27 @@
-from django import template
+from django import forms, template
 
-register = template.Library()
+register = template.Library() # если мы не зарегистрируем наши фильтры, то django никогда не узнает где именно их искать и фильтры потеряются :(
 
-@register.filter(name='NoMat')
+'''
+@register.filter(name='multiply') # регистрируем наш фильтр под именем multiply, чтоб django понимал, что это именно фильтр, а не простая функция
+def multiply(value, arg): # первый аргумент здесь — это то значение, к которому надо применить фильтр, второй аргумент — это аргумент фильтра, т.е. примерно следующее будет в шаблоне value|multiply:arg
+    return str(value) * arg # возвращаемое функцией значение — это то значение, которое подставится к нам в шаблон
+'''
 
-STOP_LIST = [
-    'мат',
-    'мат',
-    'мат',
+@register.filter(name='multiply')
+def multiply(value, arg):
+    if isinstance(value, str) and isinstance(arg, int): # проверяем, что value — это точно строка, а arg — точно число, чтобы не возникло курьёзов
+        return str(value) * arg
+    else:
+        raise ValueError(f'Нельзя умножить {type(value)} на {type(arg)}') # в случае, если кто-то неправильно воспользовался нашим тегом, выводим ошибку
+
+STOP_LIST=[
+    "mat1",
+    "mat2",
+    "mat3",
 ]
-
-    def NoMat(value, STOP_LIST):
-        value = self.cleaned_data['value']
-        for word in STOP_LIST:
-            if word in value:
-                raise forms.ValidationError("Вы позволили себе немного лишнего! Одумайтесь и исправьте текст!")
-        return value
+@register.filter(name='nomat')
+def nomat(value, STOP_LIST):
+    for world in STOP_LIST:
+        if world in value:
+    retern "***"
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,7 +1,22 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
+from django.utils import timezone
 
 class PostList(ListView):
     model = Post
-    template_name = 'templates/news/news.html'
+    template_name = 'news/news.html'
     context_object_name = 'posts'
+    ordering = ['-time_in']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = timezone.localtime(timezone.now())  # добавим переменную текущей даты time_now
+        context[
+            'value1'] = None
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'news/new.html'
+    context_object_name = 'post'
